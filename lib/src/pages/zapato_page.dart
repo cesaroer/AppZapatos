6ec0894fabc3ea4a79b7745cc1d9ee1f414b3e35
cesaroer <cesarvarcard@gmail.com>
2023-06-08 11:30:39 -0600

    Added bottom section

--- lib/src/pages/zapato_page.dart
+++ lib/src/pages/zapato_page.dart
@@ -26,7 +26,40 @@ class ZapatoPage extends StatelessWidget {
             ),
           ),
         ),
+        AgregarCarritoBtn(monto: 180.0),
       ],
     ));
+  }
+}
+
+class AgregarCarritoBtn extends StatelessWidget {
+  final double monto;
+
+  const AgregarCarritoBtn({required this.monto});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        width: double.infinity,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Row(
+          children: [
+            SizedBox(width: 20),
+            Text(
+              "\$$monto",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.w800),
+            ),
+            Spacer(),
+            OrangeBtn(text: "Add to cart"),
+            SizedBox(width: 20)
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/src/widgets/custom_widgets.dart
+++ lib/src/widgets/custom_widgets.dart
@@ -1,3 +1,5 @@
+export 'package:zapatos_app/src/widgets/orange_btn.dart';
+
 export 'package:zapatos_app/src/widgets/zapato_descript.dart';
 
 export 'package:zapatos_app/src/widgets/custom_appbar.dart';
