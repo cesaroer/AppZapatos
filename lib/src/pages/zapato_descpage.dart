d48935b261cbb35d3f341bfa62219f1b77deb6e2
cesaroer <cesarvarcard@gmail.com>
2023-06-08 17:53:15 -0600

    Added price and buy btn

--- lib/src/pages/zapato_descpage.dart
+++ lib/src/pages/zapato_descpage.dart
@@ -18,11 +18,32 @@ class ZapatoDescriptionPAge extends StatelessWidget {
                     description:
                         "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
                   ),
+                  _MontoBuyNow()
                 ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _MontoBuyNow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        margin: EdgeInsets.only(top: 15),
+        child: Row(children: [
+          Text(
+            "\$180.0",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+          ),
+          Spacer(),
+          OrangeBtn(text: "Buy now", width: 120, height: 40),
+        ]),
       ),
     );
   }
--- lib/src/widgets/orange_btn.dart
+++ lib/src/widgets/orange_btn.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 class OrangeBtn extends StatelessWidget {
   final String text;
+  final double height;
+  final double width;
 
-  const OrangeBtn({required this.text});
+  const OrangeBtn({
+    required this.text,
+    this.height = 50,
+    this.width = 150,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      width: 150,
-      height: 50,
+      width: width,
+      height: height,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(100), color: Colors.orange),
       child: Text(
